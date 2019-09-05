300e2c9928353c2a1693f47519577a8c6c5b6972
Atmaca <uic29042@contiwan.com>
2019-09-05 10:55:36 +0200

    Download aus Ordner

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, send_file
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-@app.route('/download')
-def downloadFile ():
-    path = "D:/File.txt"
-    return send_file(path, as_attachment=True)
+
+@app.route('/download/<path:filename>')
+def downloadFile (filename):
+    return send_from_directory('files', filename, as_attachment=True, cache_timeout=0)
 
 if __name__ == '__main__':
     app.run(debug=True)
